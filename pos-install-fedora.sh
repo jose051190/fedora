755cf28548558eda51aa58525c7bb06b6ae5e704
jose051190 <98242282+jose051190@users.noreply.github.com>
2024-11-05 09:53:31 -0300

    Update pos-install-fedora.sh

--- pos-install-fedora.sh
+++ pos-install-fedora.sh
@@ -47,7 +47,7 @@ done
 # Instalando pacote específico com URL
 install_flatpak_user "https://sober.vinegarhq.org/sober.flatpakref"
 
-flatpak override --env=GTK_THEME=Adwaita-dark
+sudo flatpak override --env=GTK_THEME=Adwaita-dark
 
 # Caminho para o arquivo de configuração do Chromium
 CONFIG_PATH="$HOME/.var/app/org.chromium.Chromium/config/chromium-flags.conf"
